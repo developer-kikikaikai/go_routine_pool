e2e8a1c7ff9a3be3ca8ebd53ebc57ff3cf7482f2
developer-kikikaikai <kikikaikaikai@gmail.com>
2020-03-06 22:44:22 +0900

    Remove license comment

--- go_routine_worker.go
+++ go_routine_worker.go
@@ -1,6 +1,3 @@
-// Copyright 2020 developer-kikikaikai.  All rights reserved.
-// Use of this source code is governed by a MIT style
-// license that can be found in the LICENSE file.
 package grworker
 
 import (
